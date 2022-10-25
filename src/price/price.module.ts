42a10b1afef0738df568199168c1f1d91c1dc601
nazhG <nazhirgonzalez@gmail.com>
2022-10-25 17:02:14 -0400

    schema & interfaces

--- src/price/price.module.ts
+++ src/price/price.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PriceController } from './price.controller';
+import { PriceService } from './price.service';
 
-@Module({})
+@Module({
+  controllers: [PriceController],
+  providers: [PriceService],
+})
 export class PriceModule {}
